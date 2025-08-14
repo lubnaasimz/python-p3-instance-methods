56a33d26a10ed22e15526e436cfb011856309f84
lubnaa <lubnaamuhammed@gmail.com>
2025-08-14 16:02:07 +0300

    final commit

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
 class Dog:
+    def sit(self):
+        print("The dog is sitting.")
+    def bark(self):
+        print("Woof!")
+
+wibo = Dog()
+wibo.sit()
+wibo.bark()
     # Class body goes here
 
-    #Instance method definition
-    pass
+    #Instance method definition 
--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
 class Person:
+    def talk(self):
+        print("Hello World!")
+
+    def walk(self):
+        print("The person is walking.")    
+
+polly = Person()
+polly.talk()
+polly.walk()
     # Class body goes here
 
-    #Instance method definition
-    pass
+    #Instance method definition 
